810f4f637a53d214cfd51557ef08bad5658592c5
Siddharth Gajare <124122799+siddharthgajare@users.noreply.github.com>
2026-08-22 15:49:35 +0530

    Update next.config.js to modify ESLint settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: { unoptimized: true },
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
 };
 
 module.exports = nextConfig;
